3c2c95ecf7cfc2268fa77ef99b7dfe7272b8521e
Martim Jales <mjales@c1r5s1.42lisboa.com>
2022-03-26 19:22:51 +0000

    Add distributor and char functions

--- ft_printf.c
+++ ft_printf.c
@@ -6,31 +6,13 @@
 /*   By: mjales <mjales@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/15 17:39:12 by mjales            #+#    #+#             */
-/*   Updated: 2022/03/15 20:04:34 by mjales           ###   ########.fr       */
+/*   Updated: 2022/03/26 19:09:25 by mjales           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdarg.h>
 #include <stdio.h>
 #include <unistd.h>
-
-void	ft_putnbr(int n)
-{
-	char	c;
-
-	c = n + '0';
-	write(1, &c, 1);
-}
-
-size_t	ft_strlen(const char *str)
-{
-	size_t	i;
-
-	i = 0;
-	while (str[i])
-		i++;
-	return (i);
-}
 
 char	*ft_strchr(const char *s, int c)
 {
@@ -50,6 +32,26 @@ char	*ft_strchr(const char *s, int c)
 	return (NULL);
 }
 
+int	ft_handle_flags(char flag, va_list *vl)
+{
+	if (flag == 'c')
+		return (ft_char(va_arg(*vl, int)));
+	else if (flag == 's')
+		return (ft_string(va_arg(*vl, char *)));
+	else if (flag == 'p')
+		return (ft_pointer(va_arg(*vl, unsigned long, "0123456789abcdef")));
+	else if (flag == 'i' || flag == 'd')
+		return (ft_integer(va_arg(*vl, int)));
+	else if (flag == 'x')
+		return (ft_hexa(va_arg(*vl, unsigned int), "0123456789abcdef"));
+	else if (flag == 'X')
+		return (ft_hexa(va_arg(*vl, unsigned int), "0123456789ABCDEF"));
+	else if (flag == 'u')
+		return (ft_unsigned(va_arg(*vl, unsigned int)));
+	else if (flag == '%')
+		write(1, "%%", 1);
+}
+
 int	ft_printf(const char *s, ...)
 {
 	int		i;
@@ -62,8 +64,7 @@ int	ft_printf(const char *s, ...)
 	{
 		if (s[i] == '%')
 		{
-			if (ft_strchr("cspdiuxX-+# 0", s[++i]))
-				write(1, "!", 1);
+			ft_handle_flags(s[++i], &vl);
 			n = va_arg(vl, int);
 			ft_putnbr(n);
 		}
@@ -76,6 +77,8 @@ int	ft_printf(const char *s, ...)
 
 int	main(void)
 {
-	ft_printf("ola %+d != %+04d\n", 5, 6);
-	printf("ola %+f != %+50d", -5.244, -35);
+	ft_va_test(4, 1,2,3,4);
+
+	// ft_printf("ola %+d != %+04d\n", 5, 6);
+	// printf("ola %+f != %+50d", -5.244, -35);
 }
